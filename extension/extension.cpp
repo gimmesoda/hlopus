efc78702772bed81e6be5188acaa385a21ead448
D_TWO_TWO <dprojetmail@gmail.com>
2026-02-12 22:04:20 +0300

    simplify opus_read, fix ogg bug

--- extension/extension.cpp
+++ extension/extension.cpp
@@ -19,9 +19,11 @@ static void opus_finalize(fmt_opus* o) {
 static int opus_memread(void* stream, unsigned char* ptr, int nbytes) {
 	fmt_opus* o = (fmt_opus*)stream;
 	int len = nbytes;
+
 	if (o->pos + len > o->size)
 		len = o->size - o->pos;
 	if (len <= 0) return 0;
+
 	memcpy(ptr, o->bytes + o->pos, len);
 	o->pos += len;
 	return len;
@@ -29,21 +31,24 @@ static int opus_memread(void* stream, unsigned char* ptr, int nbytes) {
 
 static int opus_memseek(void* stream, opus_int64 _offset, int whence) {
 	fmt_opus* o = (fmt_opus*)stream;
-	int offset = (int)_offset;
+	opus_int64 newpos;
+
 	switch (whence) {
 		case SEEK_SET:
-			if (offset < 0 || offset > o->size) return -1;
-			o->pos = offset;
+			newpos = _offset;
 			break;
 		case SEEK_CUR:
-			if (o->pos + offset < 0 || o->pos + offset > o->size) return -1;
-			o->pos += offset;
+			newpos = o->pos + _offset;
 			break;
 		case SEEK_END:
-			if (o->size + offset < 0 || o->size + offset > o->size) return -1;
-			o->pos = o->size + offset;
+			newpos = o->size + _offset;
 			break;
+		default:
+			return -1;
 	}
+
+	if (newpos < 0 || newpos > o->size) return -1;
+	o->pos = (int)newpos;
 	return 0;
 }
 
@@ -67,16 +72,17 @@ HL_PRIM fmt_opus* HL_NAME(opus_open)(char* bytes, int size) {
 	o->size = size;
 	o->pos = 0;
 	o->f = op_open_callbacks(o, &OPUS_CALLBACKS_MEMORY, NULL, 0, &error);
+
 	if (error != 0 || o->f == NULL)
 		return NULL;
+
 	o->finalize = opus_finalize;
 	return o;
 }
 
-HL_PRIM void HL_NAME(opus_info)(fmt_opus* o, int* bitrate, int* freq, int* samples, int* channels) {
+HL_PRIM void HL_NAME(opus_info)(fmt_opus* o, int* freq, int* samples, int* channels) {
 	const OpusHead* head = op_head(o->f, -1);
-	*bitrate = op_bitrate(o->f, -1);
-	*freq = 48000; // Opus always decodes to 48kHz
+	*freq = 48000; // opus always decodes to 48kHz
 	*channels = head->channel_count;
 	*samples = (int)op_pcm_total(o->f, -1);
 }
@@ -90,45 +96,42 @@ HL_PRIM bool HL_NAME(opus_seek)(fmt_opus* o, int sample) {
 }
 
 HL_PRIM int HL_NAME(opus_read)(fmt_opus* o, char* output, int size, int format) {
-	int ret = 0;
 	int total = 0;
 	const OpusHead* head = op_head(o->f, -1);
 	int ch = head->channel_count;
+	int bps = (format == 2) ? 2 : 4; // bytes per sample per channel
+	int samples_requested = size / (bps * ch);
 	hl_blocking(true);
-	if (format == 2) {
-		// I16 output
-		int samples_requested = size / (2 * ch);
-		while (samples_requested > 0) {
+
+	while (samples_requested > 0) {
+		int ret;
+
+		if (format == 2)
 			ret = op_read(o->f, (opus_int16*)output, samples_requested * ch, NULL);
-			if (ret < 0) { total = -1; break; }
-			if (ret == 0) break;
-			int bytes_read = ret * ch * 2;
-			total += bytes_read;
-			output += bytes_read;
-			samples_requested -= ret;
-		}
-	}
-	else {
-		// F32 output
-		int samples_requested = size / (4 * ch);
-		while (samples_requested > 0) {
+		else
 			ret = op_read_float(o->f, (float*)output, samples_requested * ch, NULL);
-			if (ret < 0) { total = -1; break; }
-			if (ret == 0) break;
-			int bytes_read = ret * ch * 4;
-			total += bytes_read;
-			output += bytes_read;
-			samples_requested -= ret;
+
+		if (ret < 0) {
+			total = -1;
+			break;
 		}
+
+		if (ret == 0) break;
+
+		int bytes_read = ret * ch * bps;
+		total += bytes_read;
+		output += bytes_read;
+		samples_requested -= ret;
 	}
+
 	hl_blocking(false);
 	return total;
 }
 
 #define _OPUS _ABSTRACT(fmt_opus)
 
-DEFINE_PRIM(_OPUS,	opus_open, _BYTES	_I32);
-DEFINE_PRIM(_VOID,	opus_info, _OPUS	_REF(_I32) _REF(_I32) _REF(_I32) _REF(_I32));
-DEFINE_PRIM(_I32,	opus_tell, _OPUS);
-DEFINE_PRIM(_BOOL,	opus_seek, _OPUS	_I32);
-DEFINE_PRIM(_I32,	opus_read, _OPUS	_BYTES _I32 _I32);
+DEFINE_PRIM(_OPUS,		opus_open, _BYTES	_I32);
+DEFINE_PRIM(_VOID,		opus_info, _OPUS	_REF(_I32) _REF(_I32) _REF(_I32));
+DEFINE_PRIM(_I32,		opus_tell, _OPUS);
+DEFINE_PRIM(_BOOL,		opus_seek, _OPUS	_I32);
+DEFINE_PRIM(_I32,		opus_read, _OPUS	_BYTES _I32 _I32);
